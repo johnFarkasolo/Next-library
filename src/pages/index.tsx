40da8953a70c1bd9a77971553a1b96c77dd446c9
farkasolo09 <ivan.yarovyi@300devs.com>
2022-07-04 12:40:27 +0200

    Add linter, prettier, i18-next,

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import type { NextPage } from 'next'
+import { useTranslation } from 'next-i18next';
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 
 const Home: NextPage = () => {
-  return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  )
+  const { t } = useTranslation('common');
+  return <h1 className="text-3xl font-bold underline">{t('homepage_title')}</h1>
 }
 
+// @ts-ignore
+export const getServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common']))
+  }
+});
 export default Home
